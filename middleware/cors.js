bf4af073b44abf2a5dbd212eff8837263897f936
Saurabh Chaudhari <139246425+Saurabh-Chaudhari-18@users.noreply.github.com>
2024-09-10 13:51:55 +0530

    Update cors.js core once again

--- middleware/cors.js
+++ middleware/cors.js
@@ -2,16 +2,22 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://sneaker-head-frontend.vercel.app',  // Replace this with your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Specify allowed methods
+  credentials: true,// If you're using cookies or other credentials
+   allowedHeaders: "Content-Type,Authorization",
+}));
 
 // If you need to specify allowed origins:
-app.use(
-  cors({
-    origin: "https://sneaker-head-frontend.vercel.app/",
-    methods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
-  })
-);
+// app.use(
+//   cors({
+//     origin: "https://sneaker-head-frontend.vercel.app/",
+//     methods: "GET,POST,PUT,DELETE",
+//     allowedHeaders: "Content-Type,Authorization",
+//      credentials: true,
+//   })
+// );
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
