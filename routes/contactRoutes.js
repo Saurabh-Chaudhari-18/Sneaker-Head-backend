6d262180800efa4436daae2199ffa8ec7eb7ff02
Saurabh Chaudhari <139246425+Saurabh-Chaudhari-18@users.noreply.github.com>
2024-09-10 16:04:49 +0530

    Update contactRoutes.js

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,20 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const Conatct = require('../models/Contact');
 const { submitContactForm } = require("../controllers/contactController");
 
 // Route to handle contact form submission
-router.post("/submit", submitContactForm) => {
-  const { email, phone, message } = req.body;
-
-  try {
-    const newContact = new Contact({ email, phone, message });
-    await newContact.save();
-    res.status(201).json({ message: 'Contact saved successfully!' });
-  } catch (error) {
-    console.error('Error saving contact:', error);
-    res.status(500).json({ error: 'An error occurred while saving the contact information.' });
-  }
-};
+router.post("/submit", submitContactForm);
 
 module.exports = router;
